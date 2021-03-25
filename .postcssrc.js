615011eb83a5e243c1e74a5bf680a6dce89423e7
[muweigg] <muweigg@gmail.com>
2021-03-25 11:21:10 +0800

    update

--- .postcssrc.js
+++ .postcssrc.js
@@ -2,17 +2,29 @@ module.exports = {
   "plugins": {
     'postcss-preset-env': {},
     // 'postcss-aspect-ratio-mini': {},
-    'postcss-px-to-viewport': {
-      viewportWidth: 750,
-      viewportHeight: 1368,
+    // 'postcss-px-to-viewport': {
+    //   viewportWidth: 750,
+    //   viewportHeight: 1368,
+		// 	unitPrecision: 6,
+		// 	mediaQuery: true,
+    //   // exclude: [/node_modules/i],
+    //   selectorBlackList: ['.ignore', '.hairlines', /\.van-loading__text/, /\.van-(?!loading)/]
+    // },
+    'postcss-pxtorem': {
+      rootValue: 108,
 			unitPrecision: 6,
 			mediaQuery: true,
+      propList: [
+        '*',
+        // '!box-shadow',
+        // '!filter*',
+        // '!border-b*',
+        // '!border-ri*',
+        // '!border-t*',
+        // '!border-le*'
+      ],
       // exclude: [/node_modules/i],
-      selectorBlackList: ['.ignore', '.hairlines', /\.van-loading__text/, /\.van-(?!loading)/]
+      selectorBlackList: ['.ignore']
     },
-    // 'postcss-write-svg': {utf8: false},
-    // 'postcss-viewport-units': {
-    //   filterRule: rule => !/.*?:{1,2}(before|after).*?\s?/i.test(rule.selector)
-    // }
   }
 }
